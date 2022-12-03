17b9803608f6a83b40a67a4445684e2484f9d459
aleksabajat2001@gmail.com <aleksabajat2001@gmail.com>
2022-12-03 16:35:11 +0100

    -project init

---
+++ main.py
@@ -0,0 +1,27 @@
+from flask import Flask, render_template, url_for
+import json
+
+app = Flask(__name__)
+
+app.config['TEMPLATES_AUTO_RELOAD'] = True
+
+
+@app.route('/home')
+def home():
+    app.logger.info("Home")
+
+    jsonData = json.load(open('config.json', 'r'))
+
+    
+
+    return render_template("questionnaire.html", pages=pages)
+
+
+@app.route('/admin')
+def admin():
+    app.logger.info("Admin")
+
+    return render_template("admin.html")
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5000, host='0.0.0.0')
