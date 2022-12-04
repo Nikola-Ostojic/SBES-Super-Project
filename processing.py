b2a17b77e71d093104ab95e20969679a053cd96b
aleksabajat2001@gmail.com <aleksabajat2001@gmail.com>
2022-12-04 19:33:14 +0100

    -admin create and read

--- processing.py
+++ processing.py
@@ -22,17 +22,17 @@ def parse_pages(pages):
     return result
 
 def parse_answer(answer):
-    id = answer['id'] if 'id' in answer else None
+    index = answer['index']
     text = answer['text']
     weight = answer['weight']
-    return Answer(text,weight, id)
+    return Answer(index,text,weight)
 
 def parse_question(question):
-    id = question['id'] if 'id' in question else None
+    index = question['index']
     q = question['question']
     answer_type = question['answerType']
     weight_type = question['weightType']
-    return Question(q,answer_type,weight_type,id)
+    return Question(index,q,answer_type,weight_type)
 
 def parse_page(page):
     index = page['index']
@@ -44,7 +44,7 @@ def parse_page(page):
 def add_page_to_json(config_name,index,title,formula):
     try:
         config_data = json.load(open(config_name, 'r'))
-        data = {"index":int(index),"title":str(title),"formula":str(formula)}
+        data = {"index":int(index),"title":str(title),"formula":str(formula),"questions":[]}
         config_data['pages'].append(data)
 
         json_data = json.dumps(config_data)
@@ -54,3 +54,60 @@ def add_page_to_json(config_name,index,title,formula):
     except Exception as e:
         print(str(e), flush=True)
         return False
+
+def add_question_to_json(config_name,page_index,question_index,question,answer_type,weight_type):
+    try:
+        config_data = json.load(open(config_name,'r'))
+        data = {"index":int(question_index),"question":str(question),"answerType":str(answer_type),"weightType":str(weight_type),"answers":[]}
+
+        found_index, page_idx = get_page_json(config_data['pages'],page_index)
+        if found_index == False: raise Exception("Page index not found.")
+     
+        config_data['pages'][int(page_idx)]['questions'].append(data)
+        
+        json_data = json.dumps(config_data)
+
+        with open(config_name, "w") as file:
+            file.write(json_data)
+        return True
+        
+    except Exception as e:
+        print(str(e), flush=True)
+        return False
+
+def add_answer_to_json(config_name,page_index,question_index,answer_index,text,weight):
+    try:
+        config_data = json.load(open(config_name,'r'))
+        data = {"index":int(answer_index),"text":str(text),"weight":float(weight)}
+
+        found_index, page_idx = get_page_json(config_data['pages'], page_index)
+        if found_index == False: raise Exception("Page index not found.")
+
+        found_index, question_idx = get_page_json(config_data['pages'][page_idx]['questions'],question_index)
+        if found_index == False: raise Exception("Question index not found.")
+        
+        config_data['pages'][int(page_idx)]['questions'][int(question_idx)]['answers'].append(data)
+        json_data = json.dumps(config_data)
+
+        with open(config_name, "w") as file:
+            file.write(json_data)
+        return True
+        
+    except Exception as e:
+        print(str(e), flush=True)
+        return False 
+
+
+def get_page_json(items,index):    
+    counter = 0
+    for item in items:
+        if int(item['index']) == int(index):
+            return True,counter
+        counter += 1
+    
+    return False,counter
+
+
+
+
+    
--- static/js/admin.js
+++ static/js/admin.js
@@ -1,6 +1,6 @@
 function addPage(){
     data = document.getElementById("page-form");
-    formData = new FormData(data);
+    formData = new FormData(data);    
 
     $.ajax({
         type: "POST",
@@ -8,9 +8,50 @@ function addPage(){
         data: formData,
         contentType: false,
         processData: false,
-        success: function (data) {
-            console.log(data)
+        success: function (response) {
+            console.log(response)
         }
     });
 }
+
+function addQuestion(index){
+    n = "question-form-" + index
+    data = document.getElementById(n)
+    formData = new FormData(data);
+    formData.append('pageIndex', index)
+
+    $.ajax({
+        type: "POST",
+        url: "/addQuestion",
+        data: formData,
+        contentType: false,
+        processData: false,
+        success: function (response) {
+            console.log(response)
+        }
+    });
+}
+
+function addAnswer(pageIndex,questionIndex){
+    n = "answer-form-" + pageIndex + "-" + questionIndex
+    data = document.getElementById(n);
+    formData = new FormData(data);
+    formData.append('pageIndex',pageIndex);
+    formData.append('questionIndex', questionIndex);
+
+    $.ajax({
+        type: "POST",
+        url: "/addAnswer",
+        data: formData,
+        contentType: false,
+        processData: false,
+        success: function (response) {
+            console.log(response)
+        }
+    });
+}
+
+
+
+
 
