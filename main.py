a325045115643e0808fc55984b3bde6e7c5c0985
aleksabajat2001@gmail.com <aleksabajat2001@gmail.com>
2022-12-11 23:39:50 +0100

    editing adding

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, url_for,request, session, redirect
 from flask_session import Session
 from calculate import calculate_result
-from processing import process,add_page_to_json,add_question_to_json, add_answer_to_json
+from processing import process, edit_page_json,edit_question_json, edit_answer_json, add_answer_to_json
 from models import Page
 
 app = Flask(__name__)
@@ -21,7 +21,7 @@ def home():
     # else:  
     #     pages = session.get('pages')
     
-    _, pages = process(CONFIG_NAME)
+    pages = process(CONFIG_NAME)
     
     app.logger.info(pages)
     app.logger.info(type(pages))
@@ -32,44 +32,83 @@ def home():
 
 @app.route('/admin')
 def admin():    
-    _ , pages = process(CONFIG_NAME)
+    pages = process(CONFIG_NAME)
     return render_template("admin.html", pages=pages)
 
-@app.route('/addPage', methods=["POST"])
-def add_page():
-    index = request.form.get('pageIndex')
+@app.route('/editPage', methods=["POST"])
+def edit_page():
+    page_id = request.form.get('pageId')
+    index = request.form.get('index')
     title = request.form.get('title')
-    formula = request.form.get('formula')
 
-    success = add_page_to_json(CONFIG_NAME,index,title,formula)
+    success = edit_page_json(CONFIG_NAME, page_id, index, title)
 
     response = app.response_class(response="OK" if success else "ERR",status=200 if success else 400)
-    return response
+    return response    
 
-@app.route('/addQuestion', methods=["POST"])
-def add_question():
-    page_index = request.form.get('pageIndex')
-    question_index = request.form.get('questionIndex')
+@app.route('/editQuestion', methods=["POST"])
+def edit_question():
+    page_id = request.form.get('pageId')
+    question_id = request.form.get('questionId')
+    question_index = request.form.get('index')
     question = request.form.get('question')
     answer_type = request.form.get('answerType')
-    weight_type = request.form.get('weightType')
 
-    app.logger.info(page_index)    
+    success = edit_question_json(CONFIG_NAME, page_id, question_id,question_index,question,answer_type)
+    response = app.response_class(response="OK" if success else "ERR",status=200 if success else 400)
+    return response    
 
-    success = add_question_to_json(CONFIG_NAME,page_index,question_index,question,answer_type,weight_type)
+@app.route('/editAnswer', methods=['POST'])
+def edit_answer():
+    page_id = request.form.get('pageId')
+    question_id = request.form.get('questionId')
+    answer_id = request.form.get('answerId')
+    answer_index = request.form.get('index')
+    text = request.form.get('answer')
+    weight = request.form.get('weight')
+ 
+
+    success = edit_answer_json(CONFIG_NAME, page_id,question_id, answer_id,answer_index,text,weight)
 
     response = app.response_class(response="OK" if success else "ERR",status=200 if success else 400)
-    return response
+    return response       
+
+
+# @app.route('/addPage', methods=["POST"])
+# def add_page():
+#     index = request.form.get('pageIndex')
+#     title = request.form.get('title')
+#     formula = request.form.get('formula')
+
+#     success = add_page_to_json(CONFIG_NAME,index,title,formula)
+
+#     response = app.response_class(response="OK" if success else "ERR",status=200 if success else 400)
+#     return response
+
+# @app.route('/addQuestion', methods=["POST"])
+# def add_question():
+#     page_index = request.form.get('pageIndex')
+#     question_index = request.form.get('questionIndex')
+#     question = request.form.get('question')
+#     answer_type = request.form.get('answerType')
+#     weight_type = request.form.get('weightType')
+
+#     app.logger.info(page_index)    
+
+#     success = add_question_to_json(CONFIG_NAME,page_index,question_index,question,answer_type,weight_type)
+
+#     response = app.response_class(response="OK" if success else "ERR",status=200 if success else 400)
+#     return response
 
 @app.route('/addAnswer', methods=["POST"])
 def add_answer():
-    page_index = request.form.get('pageIndex')
-    question_index = request.form.get('questionIndex')
-    answer_index = request.form.get('answerIndex')
-    text = request.form.get('text')
+    page_id = request.form.get('pageId')
+    question_id = request.form.get('questionId')
+    answer_index = request.form.get('index')
+    text = request.form.get('answer')
     weight = request.form.get('weight')
 
-    success = add_answer_to_json(CONFIG_NAME, page_index,question_index,answer_index,text,weight)
+    success = add_answer_to_json(CONFIG_NAME, page_id,question_id,answer_index,text,weight)
 
     response = app.response_class(response="OK" if success else "ERR",status=200 if success else 400)
     return response
@@ -80,7 +119,7 @@ def result():
     # if not session.get("pages"):
     #     return "Session expired, please fill out the form again. Sorry for the inconvenience."
 
-    _, pages = process(CONFIG_NAME)
+    pages = process(CONFIG_NAME)
 
     if request.method == 'GET':
         if(session['result']):
