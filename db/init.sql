4b0586e7b459d49ebe548a037e7ef1462d61d597
AleksaBajat <aleksabajat2001@gmail.com>
2023-05-28 10:43:11 +0200

    docker compose init sql

--- db/init.sql
+++ db/init.sql
@@ -3,6 +3,15 @@ CREATE USER 'sbes'@'localhost' IDENTIFIED BY 'sbesftn';
 create database if not exists sbesproject;
 use sbesproject;
 
+GRANT ALL PRIVILEGES ON sbesproject.* TO 'sbes'@'localhost';
+
 ALTER DATABASE sbesproject CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci;
 
-GRANT ALL PRIVILEGES ON sbesproject.* TO 'sbes'@'localhost';
+CREATE TABLE users (
+    Id INT AUTO_INCREMENT PRIMARY KEY,
+    Username varchar(255) UNIQUE,
+    Password varchar(255)
+);
+
+INSERT INTO users (Username, Password) VALUES ('ftn', 'sbesadminftn')
+
